a04a0af7294ad934fc885beca8e8c4cd8ca3b112
Jobizil <ugbemjob57@gmail.com>
2023-02-10 00:46:14 +0100

    Added day 2 oop

--- zero-to-mastery/password-checker.py
+++
@@ -1,7 +0,0 @@
-username = input('>>> Username: ')
-password = str(input('>>> Password: '))
-
-password_length = len(password)
-hash_password = 10 * '*'
-
-print(f'Dear {username.capitalize()}, your password {hash_password} is {password_length} letters long! ')
